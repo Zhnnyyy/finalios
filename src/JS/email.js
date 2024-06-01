11e848254a74eeef5be8d9d80f7d307704c6c13b
Zhnnyyy <112181655+Zhnnyyy@users.noreply.github.com>
2024-06-01 12:52:00 +0800

    Update email.js

--- src/JS/email.js
+++ src/JS/email.js
@@ -3,11 +3,7 @@ import { Fetch } from "./model/bridge.js";
 import { showMessage, loading } from "./model/MyAlert.js";
 $(() => {
   const validator = navigator.userAgent;
-   if (!validator.match(/iPhone/i)) {
-     window.location.href = "page.html";
-    return;
-  }
-  //HAHA
+   
   if (
     !localStorage.getItem("setEmail") ||
     localStorage.getItem("setEmail") == null
